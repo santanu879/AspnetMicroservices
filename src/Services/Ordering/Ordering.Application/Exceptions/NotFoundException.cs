d05af65b3d1786b2245c0aee66e00a4ea62b6957
santanu879 <santanu879@gmail.com>
2023-02-10 22:52:50 +0530

    changes oder api

--- src/Services/Ordering/Ordering.Application/Exceptions/NotFoundException.cs
+++ src/Services/Ordering/Ordering.Application/Exceptions/NotFoundException.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Ordering.Application.Exception
+namespace Ordering.Application.Exceptions
 {
     public class NotFoundException:ApplicationException
     {
